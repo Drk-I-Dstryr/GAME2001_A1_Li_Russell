8ad12f3c5ee838396df91c89dd5e30a1694beda3
Russell Li <russell.li@rocketmail.com>
2021-10-08 19:16:55 -0400

    Started working on main and added the modifications to the expand function
    Started working on main and added the modifications to the expand function

--- GAME2001_A1_Li_Russell/GAME2001_A1_Li_Russell/Main.cpp
+++ GAME2001_A1_Li_Russell/GAME2001_A1_Li_Russell/Main.cpp
@@ -5,7 +5,29 @@ using namespace std;
 
 int main()
 {
+	int numSpaces = 5;
+	UnorderedArray<int> array1(numSpaces);
+	OrderedArray<int> array2(numSpaces);
 
+	array1.push2(0);
+	array1.push2(0);
+	array1.push2(0);
+	array1.push2(0);
+	array1.push2(0);
+
+	array2.push(0);
+	array2.push(0);
+	array2.push(0);
+	array2.push(0);
+	array2.push(0);
+
+	int choice2 = 1;
+	int choice = 5;
+
+	for (int i = 0; choice2 != 0; i++)
+	{
+		cout << "What number do you want to enter into the array? Enter 0 to exit: ";
+	}
 
 	return 0;
 }
